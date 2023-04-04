02ff4840e9c35dfbe1db396441cc886b57dcb187
samstha07 <samshrestha85@gmail.com>
2023-04-04 12:19:27 +0545

    updated eslintrc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next';
 import ButtonComponent from '@/components/button.component';
 
 const Home: NextPage = () => {
+
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello</h1>
